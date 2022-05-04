319987d31ec6e4a37858575a38a956ba75b50afe
marmooo <marmooo@users.noreply.github.com>
2022-05-04 09:10:09 +0900

    update db

--- reduce.py
+++ reduce.py
@@ -1,11 +1,18 @@
 words = {}
-with open("words.lst") as f:
+with open("siminym-zh/all.lst") as f:
     for line in f:
         word = line.split(",", 1)[0]
         words[word] = True
 
+count = 0
+with open("cc.zh.300.vec") as f:
+    for line in f:
+        word = line.split(" ", 1)[0]
+        if word in words:
+            count += 1
+
 with open("cc.zh.300-small.vec", "w") as fout:
-    fout.write(str(len(words)) + " 300\n")
+    fout.write(str(count) + " 300\n")
     with open("cc.zh.300.vec") as fin:
         for line in fin:
             word = line.split(" ", 1)[0]
