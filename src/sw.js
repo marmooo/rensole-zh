439fc6359f2c2647b100a792b7f0278369ec5754
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:01:12 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-08 11:03";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/rensole-zh/",
   "/rensole-zh/index.js",
@@ -6,7 +6,7 @@ var urlsToCache = [
   "/rensole-zh/sql.js-httpvfs/sql-wasm.wasm",
   "/rensole-zh/sql.js-httpvfs/sqlite.worker.js",
   "/rensole-zh/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
   "https://cdn.jsdelivr.net/npm/mathjs@10.4.3/lib/browser/math.js",
 ];
