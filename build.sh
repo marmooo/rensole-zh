d4ed336f3db15a190e89b409a5435496449c1a39
marmooo <marmooo@users.noreply.github.com>
2023-06-22 10:09:08 +0900

    use deno_emit

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ drop-inline-css -r docs -o docs
 minify -r docs -o .
 cp node_modules/sql.js-httpvfs/dist/sqlite.worker.js docs/sql.js-httpvfs/
 cp node_modules/sql.js-httpvfs/dist/sql-wasm.wasm docs/sql.js-httpvfs/
-deno bundle src/index.js > docs/index.js
+deno run -A build.js ./src/index.js > docs/index.js
+minify docs/index.js -o docs/index.js
