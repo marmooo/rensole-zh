8ca6a5c8055133deccb53c96946dc6f1aca36a44
marmooo <marmooo@users.noreply.github.com>
2025-03-30 11:31:47 +0900

    rye run

--- build-dict.sh
+++ build-dict.sh
@@ -1,5 +1,5 @@
 deno run --allow-read --allow-write game.js
-python reduce.py
-python -m pymagnitude.converter \
+rye run python reduce.py
+rye run python -m pymagnitude.converter \
   -i cc.zh.300-small.vec \
   -o cc.zh.300-small.magnitude
